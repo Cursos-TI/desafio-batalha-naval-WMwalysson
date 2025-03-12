1b93ae711fb807a9b5a5ba11efc3c6ee7e4b5abe
WMwalysson <202502092041@alunos.estacio.br>
2025-03-12 17:28:19 +0000

    Desafio: nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,88 +3,81 @@
 #define TAMANHO 10
 #define NAVIO 3
 
+
 void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
-  
     for (int i = 0; i < TAMANHO; i++) {
+
         for (int j = 0; j < TAMANHO; j++) {
             tabuleiro[i][j] = 0;
  }
-}
+ }
 }
 
 void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
-  printf("   A B C D E F G H I J\n");
+    printf("   A B C D E F G H I J\n");
     for (int i = 0; i < TAMANHO; i++) {
-      printf("%2d ", i + 1);
-
+        printf("%2d ", i + 1);
         for (int j = 0; j < TAMANHO; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
-
- printf("\n");
+        printf("\n");
  }
 }
 
-int podePosicionarHorizontal(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna) {
-    if (coluna + NAVIO > TAMANHO) return 0;
+
+
+int podePosicionar(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcao) {
     for (int i = 0; i < NAVIO; i++) {
-        if (tabuleiro[linha][coluna + i] != 0) return 0;
-         }
-    return 1;
+         int l = linha + (direcao == 1 ? i : (direcao == 3 ? -i : 0));
+         int c = coluna + (direcao == 0 ? i : (direcao == 2 ? -i : 0));
+        
+        if (l < 0 || l >= TAMANHO || c < 0 || c >= TAMANHO || tabuleiro[l][c] != 0) {
+            return 0;
 }
-
-
-int podePosicionarVertical(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna) {
-    if (linha + NAVIO > TAMANHO) return 0;
-    for (int i = 0; i < NAVIO; i++) {
-        if (tabuleiro[linha + i][coluna] != 0) return 0;
     }
     return 1;
 }
 
-
-
-
-void posicionarNavioHorizontal(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna) {
-    if (podePosicionarHorizontal(tabuleiro, linha, coluna)) {
+void posicionarNavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcao) {
+         if (podePosicionar(tabuleiro, linha, coluna, direcao)) {
         for (int i = 0; i < NAVIO; i++) {
-            tabuleiro[linha][coluna + i] = NAVIO;
-            printf("navio horizontal em: (%d, %c)\n", linha + 1, 'A' + coluna + i);
-    }
+            int l = linha + (direcao == 1 ? i : (direcao == 3 ? -i : 0));
+            int c = coluna + (direcao == 0 ? i : (direcao == 2 ? -i : 0));
+            tabuleiro[l][c] = NAVIO;
+        }
     } else {
-        printf("Erro: Não é possivel posiciona o navio horizontal em (%d, %c).\n", linha + 1, 'A' + coluna);
-    }
-    }
-
-void posicionarNavioVertical(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna) {
-    if (podePosicionarVertical(tabuleiro, linha, coluna)) {
-        for (int i = 0; i < NAVIO; i++) {
-            tabuleiro[linha + i][coluna] = NAVIO;
-            printf("Navio vertical em: (%d, %c)\n", linha + i + 1, 'A'+ coluna);
-    }
-    } else {
-        printf("Erro: Não é possível posicionar o navio vertical em (%d, %c).\n", linha + 1, 'A' + coluna);
-    }
+        printf("Erro: Não é possível posicionar o navio em (%d, %c).\n", linha + 1, 'A' + coluna);
  }
+}
 
-    int main() {
-    int tabuleiro[TAMANHO][TAMANHO];
-    inicializarTabuleiro(tabuleiro);
 
-    // Posições iniciais dos navios
-    int linhaH = 1, colunaH = 1; // navio horizontal
 
-    int linhaV = 2, colunaV = 3; // posição do navio vertical
 
-    // Posicionando os navios no tabuleiro
-    posicionarNavioHorizontal(tabuleiro, linhaH, colunaH);
+int main() {
+         int tabuleiro[TAMANHO][TAMANHO];
+         inicializarTabuleiro(tabuleiro);
 
-    posicionarNavioVertical(tabuleiro, linhaV, colunaV);
 
-    // Exibindo o tabuleiro
-    printf("\n### Tabuleiro batalha naval ###\n");
+      // Posicionando dois navios horizontal/vertical
+
+    posicionarNavio(tabuleiro, 1, 1, 0); // Horizontal
+    posicionarNavio(tabuleiro, 2, 3, 1); // Vertical
+
+
+     // Posicionando dois navios diagonais
+
+    posicionarNavio(tabuleiro, 5, 5, 0); // Diagonal principal
+    posicionarNavio(tabuleiro, 7, 7, 2); // Diagonal secundária
+
+
+     // Exibir tabuleiro
+
+    printf("\n### Tabuleiro Batalha Naval ###\n");
     exibirTabuleiro(tabuleiro);
-   
+
+
+
+
 
 
 
